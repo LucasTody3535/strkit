20be6f64ec8c1dea401d1bd3b1f2e43e0d6b2e53
Toodie <lucas.vtodt@gmail.com>
2024-11-22 03:19:35 -0300

    implement header STATUS_CODES_H

---
+++ include/status_codes.h
@@ -0,0 +1,10 @@
+#ifndef STATUS_CODES_H
+#define STATUS_CODES_H
+
+    typedef enum {
+        NULL_POINTER = 301,
+        EQUALS = 201,
+        NOT_EQUALS = 202,
+    } STRING_OPERATION_CODE;
+
+#endif
